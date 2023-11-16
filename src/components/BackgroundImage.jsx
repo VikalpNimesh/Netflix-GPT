4646ce97e4b317c82cd393d7ac8e0dc220df0f8f
Vikalp Nimesh <140145370+VikalpNimesh@users.noreply.github.com>
2023-11-16 10:31:54 +0530

    Update BackgroundImage.jsx

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -2,8 +2,8 @@ import { BG_IMG } from '../utils/constant'
 
 const BackgroundImage = () => {
   return (
-    <div className="absolute object-cover h-screen">
-    <img className=" h-screen object-cover" src={BG_IMG} alt="background-img" />
+    <div className="absolute ">
+    <img className=" h-screen w-screen object-cover" src={BG_IMG} alt="background-img" />
   </div>
   )
 }
